c4316e28787c8743e5f6d517ea5fbc467e232995
Dennis Muhlestein <djmuhlestein@gmail.com>
2010-02-17 06:41:14 -0700

    Add MS Money Report mapping to sample mappings.

--- src/csv2ofx/mappings.py
+++ src/csv2ofx/mappings.py
@@ -217,5 +217,40 @@ ubs = {
     }
 }
 
+def msmoney_memo(row,grid):
+    memo=fromCSVCol(row,grid,'Memo') # sometimes None
+    cat=fromCSVCol(row,grid,'Category')
+    cls=fromCSVCol(row,grid,'Projects')
+    if len(memo)>0:
+        return "%s - %s - %s" % ( memo, cat, cls )
+    return "%s - %s" % (cat, cls)
 
-all_mappings = {'Yodlee':yodlee, 'Credit Union':cu, 'UBS':ubs}
+msmoneyrep = {
+
+    'OFX':{
+        'skip':lambda row,grid: fromCSVCol(row,grid,'Split Type') == 'Split',
+        'BANKID':lambda row,grid: fromCSVCol(row,grid,'Account Name').split(' - ')[0],
+        'ACCTID':lambda row,grid: fromCSVCol(row,grid,'Account Name').split(' - ')[-1],
+        'DTPOSTED':lambda row,grid: toOFXDate(fromCSVCol(row,grid,'Date')),
+        'TRNAMT':lambda row,grid: fromCSVCol(row,grid,'Amount'),
+        'FITID':lambda row,grid: fromCSVCol(row,grid,'Num'),
+        'PAYEE':lambda row,grid: fromCSVCol(row,grid,'Payee'),
+        'MEMO':lambda row,grid: msmoney_memo(row,grid),
+        'CURDEF':lambda row,grid: fromCSVCol(row,grid,'Currency'),
+        'CHECKNUM':lambda row,grid: fromCSVCol(row,grid,'Num')
+    },
+    'QIF':{
+        'split':lambda row,grid: fromCSVCol(row,grid,'Date') == '', #split should be determined by absence of date and other fields.
+        'Account':lambda row,grid: fromCSVCol(row,grid,'Account'),
+        'AccountDscr':lambda row,grid: fromCSVCol(row,grid,'Account'),
+        'Date':lambda row,grid: toOFXDate(fromCSVCol(row,grid,'Date')),
+        'Payee':lambda row,grid: parse_payee(row,grid),
+        'Memo':lambda row,grid: fromCSVCol(row,grid,'C') + ': ' + fromCSVCol(row,grid,'Memo'),
+        'Category':lambda row,grid: fromCSVCol(row,grid,'Category'),
+        'Class':lambda row,grid: fromCSVCol(row,grid,'Projects'),
+        'Amount':lambda row,grid: fromCSVCol(row,grid,'Amount'),
+        'Number':lambda row,grid: fromCSVCol(row,grid,'Num')
+    }
+}
+
+all_mappings = {'Yodlee':yodlee, 'Credit Union':cu, 'UBS':ubs, 'MS Money Report (CSV)':msmoneyrep }
